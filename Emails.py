bbf7df1ab8fc0ab74d6489ca6c3ec562981462e2
hardikraval1324 <hardik1324@gmail.com>
2021-09-14 14:34:20 +0530

    Added email

---
+++ Emails.py
@@ -0,0 +1,60 @@
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import json
+
+# The mail addresses and password
+sender_address = 'freshers.unofficial2021@gmail.com'
+sender_pass = 'erYAvhAaPaGQ5ju'
+# Setup the MIME
+
+# fetch email into this dynamic list
+r = open("reciepents.json",)
+reciepents = json.load(r)
+r.close()
+
+# this function sends the email
+def sendmail(sender_address, receiver_address, subject, content, receiver_name, receiver_id):
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    # The subject line
+    message['Subject'] = subject
+    # The body and the attachments for the mail
+    message.attach(MIMEText(content, 'plain'))
+    attach_file_name = './Test QR/'+str(receiver_id)+'.png'
+    with open(attach_file_name, 'rb') as f:
+        img_data = f.read()
+    image = MIMEImage(img_data, name=os.path.basename(attach_file_name))
+    message.attach(image)
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+    # login with mail_id and password
+    session.login(sender_address, sender_pass)
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print(f"Mail Sent to {receiver_name}")
+
+
+subject = '✨ Freshers 2021!'
+
+# makes the content
+
+def func_content_final(content):
+    greeting = "Hello " + reciever_name + ",\n"
+    signature = "\n\nps - Don't share your QR codes with anyone xD"
+    content_ = greeting + content + signature
+    return content_
+
+# for sending mails in a loop
+for i in range(len(reciepents)):
+    content = '''Download the below QR code and don't forget to bring it on 18th!'''
+    reciever_name = reciepents[i]["name"]
+    content_final = func_content_final(content)
+    sendmail(sender_address, reciepents[i]["email"],
+             subject, content_final, reciever_name,i+1)
+    print(f'sent to {reciepents[i]["name"]}')
--- Gsheets.py
+++ Gsheets.py
@@ -3,28 +3,40 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 import pyqrcode
 import jwt
+import json
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("test sheet").sheet1
-
 data = sheet.get_all_records() #getting all the values from Spreedsheet
-col1 = sheet.col_values(2)
-col = sheet.col_values(6)
-pprint([col1, col])
-for i in range(len(data)):
-     encoded_jwt = jwt.encode({"Name": data[i]["Name"], "U_Code": data[i]["U_Code"] , "Contact No": data[i]["Contact No"]}, "H2O2002", algorithm="HS256")
-     pprint(encoded_jwt)
-     qr = pyqrcode.create(encoded_jwt)
-     qr.png(str(i)+".png", scale=8)
-     decoded = jwt.decode(encoded_jwt, "H2O2002", algorithms=["HS256"])
-     pprint(decoded)
+name = sheet.col_values(2)
+email = sheet.col_values(8)
+# loop for printing name in json format
+def making_listToJson():
+     res = []
+     for i in range(1, len(data) + 1):
+          temp = {"name": name[i],
+                  "email": email[i]}
+          res.append(temp)
+     print(res)
+     jsonFile = open("reciepents.json", "w")
+     jsonFile.write(json.dumps(res))
+     jsonFile.close()
 
-"""
- logic to update the cell 
-   if(ture):
-       sheet.update_cell(x,y, "Checked IN")
-   else: 
-      sheet.update_cell(x,y, "empty string")
-"""
+def making_qr():
+     for i in range(len(data)):
+          encoded_jwt = jwt.encode(
+               {"Name": data[i]["Name"], "U_Code": data[i]["U_Code"], "Contact No": data[i]["Contact No"]},
+               "ef61152cce1d66d77cc0fe52d4996271ec9d74f33c891c23c2fa445b1a3b2f15780a1b35848a3a814e11144fe339eba7f8d049fb892f42121715c454d323b5c4",
+               algorithm="HS256")
+          pprint(encoded_jwt)
+          qr = pyqrcode.create(encoded_jwt)
+          qr.png(str(i+1) + ".png", scale=8)
+          decoded = jwt.decode(encoded_jwt, "ef61152cce1d66d77cc0fe52d4996271ec9d74f33c891c23c2fa445b1a3b2f15780a1b35848a3a814e11144fe339eba7f8d049fb892f42121715c454d323b5c4", algorithms=["HS256"])
+          pprint(decoded)
+
+
+making_listToJson()
+
+
